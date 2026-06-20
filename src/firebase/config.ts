1b4f0f0dce504707b273ba5afe8aaa803af9112a
aniketsahu07 <aniketsahu150707@gmail.com>
2026-06-20 10:51:51 +0530

    fix(security): resolve public leak of firebase API key by using env variable

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5469230958-a8a21",
   "appId": "1:593013670944:web:b166d2a851ee442c4c2845",
-  "apiKey": "AIzaSyD4abjTTKBxHRvCI8d6ZcyU-EPnArlk60E",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
   "authDomain": "studio-5469230958-a8a21.firebaseapp.com",
   "storageBucket": "studio-5469230958-a8a21.appspot.com",
   "measurementId": "",
